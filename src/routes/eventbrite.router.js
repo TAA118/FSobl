74277bbee8dfe725897aa96be51ed651a2f17f48
TAA118 <ado342312@gmail.com>
2026-04-29 19:19:38 -0300

    Eliminar eventbrite

--- src/routes/eventbrite.router.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import * as ticketmasterController from '../controllers/ticketmaster.controller.js';
-
-export const ticketmasterRouter = express.Router();
-
-// Ruta pública para obtener eventos por ciudad
-ticketmasterRouter.get('/ciudad/:ciudad', ticketmasterController.obtenerEventosPorCiudad);
